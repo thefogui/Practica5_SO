d23dd591222543aac05dfcb57e1fa7b8cb793bcf
thefogui <vitorthefogui@gmail.com>
2017-05-23 19:56:09 +0200

    comentarios adcionados

--- malloc.c
+++ malloc.c
@@ -1,7 +1,7 @@
-p_block first_element = NULL;
-p_block last_element  = NULL;
+p_block first_element = NULL; //apunta al primer bloc reservat
+p_block last_element  = NULL; //apunta al darrer element de la llista
 
-void *malloc(size_t size) 
+void *malloc(size_t size)
 {
     void *p;
     p_block block;
@@ -11,15 +11,16 @@ void *malloc(size_t size)
     }
 
     if (first_element){
-        block = cercar_bloc_lliure(size);
+        block = cercar_bloc_lliure(size);//mira si a la llista de blocs disponibles n'hi algun
+                                        // que sigui prou gran
         if (block) { // block found
             block->free = 0;
-        } else {     // no block found
+        } else { // si no hi bloc es demana espai(demana espai similar al sbrk(size))
             block = demanar_espai(size);
             if (!block)
                 return (NULL);
         }
-    } 
+    }
     else  // This is done the first time malloc is called
     {
         block = demanar_espai(size);
@@ -29,6 +30,27 @@ void *malloc(size_t size)
     }
 
     p = (void *) block;
-    return (p + META_SIZE); 
+    return (p + META_SIZE);
 }
 
+//Ha de posar el valor 'free' a 1
+void free(void *ptr){
+    if(!ptr) //si es NULL
+        return; //es ignora la llamada
+    struct p_block* current = (struct p_block*)ptr -1;
+    current->free = 1;
+}
+
+//permet reservar varis elements de memoria i els deixa inicialitzats a zero
+void calloc(size_t nelem, size_t elsiza){
+
+}
+
+//reajusta la mida d'un bloc de momemoria obtingut amb malloc a una nova mida
+ void *realloc(void *ptr, size_t size){
+     if(!ptr) //si es null el punter actua como una malloc normal i corrent
+        return malloc(size);
+     //si la mida que demananen es suficient amb el bloc que ja te reservat no fa res
+     //en cas contrari reservar espai per un bloc y usar la funcion memcpy per
+     //copiar el contigut d'un bloc en un altre
+ }
--- struct.h
+++ struct.h
@@ -1,12 +1,14 @@
 typedef struct m_block *p_block;
 
+/**
+* next serveix per encadenar el bloc
+*
+**/
+//meta data
 struct m_block {
-    size_t  size;
-    p_block next;
-    int     free;
+    size_t  size; //mida del bloc en bytes
+    p_block next; //adreça del seguent bloc
+    int     free; //indicador de que si el bloc esta ocupat o no
 };
 
 #define META_SIZE  sizeof(struct m_block)
-
-
-
