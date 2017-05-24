1533d34b4c216962ea0e697788d21b40cc7e121f
thefogui <vitorthefogui@gmail.com>
2017-05-24 11:43:35 +0200

    exemple.c funcional

--- exemple.c
+++ exemple.c
@@ -13,8 +13,20 @@ int main()
 
     for(i = 0; i < 10; i++)
         printf("%d\n", p[i]);
+	
+	p = (int *) realloc(p, 11 * sizeof(int));
+	for(i = 0; i < 11; i++)
+        p[i] = i;
+	for(i = 0; i < 11; i++)
+        printf("%d\n", p[i]);
 
     free(p);
+	p = (int *) calloc(10, sizeof(int));
+	for(i = 0; i < 10; i++)
+        p[i] = i;
 
+    for(i = 0; i < 10; i++)
+        printf("%d\n", p[i]);
+	free(p);
     return 0;
 }
